7f9f5bd3d0955c962f4fbde6382eb6561677ea27
NoOnE1 <ashmitnur@gmail.com>
2025-08-15 08:58:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.mynextapp',
+  appName: 'my-next-app',
+  webDir: 'out'
+};
+
+export default config;
